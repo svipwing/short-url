91e6b9ccaff60ffa2954702c92d699e11d59dbb4
技术云 <3260130869@qq.com>
2023-07-02 13:18:31 +0000

    [update] 代码规范修改
    Signed-off-by: 技术云 <3260130869@qq.com>

--- admin.php
+++ admin.php
@@ -30,11 +30,12 @@ EOF;
 }
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+
 if(! $conn )
 {
     die('连接失败: ' . mysqli_error($conn));
 }
-// 设置编码，防止中文乱码
+
 mysqli_query($conn , "set names utf8");
  
 $sql = 'SELECT num, url, ip, add_date
@@ -42,12 +43,15 @@ $sql = 'SELECT num, url, ip, add_date
  
 mysqli_select_db( $conn, $dbname );
 $retval = mysqli_query( $conn, $sql );
+
 if(! $retval )
 {
     die('无法读取数据: ' . mysqli_error($conn));
 }
+
 echo '<h2>admin后台</h2>';
 echo '<table border="1"><tr><td>编号</td><td>URL地址</td><td>用户IP地址</td><td>添加日期</td></tr>';
+
 while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
 {
     echo "<tr><td> {$row['num']}</td> ".
@@ -56,7 +60,9 @@ while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
          "<td>{$row['add_date']} </td> ".
          "</tr>";
 }
+
 echo '</table></center>';
+
 mysqli_free_result($retval);
 mysqli_close($conn);
 ?>
