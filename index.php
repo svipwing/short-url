32dd220760754f3740eab5cd3a3e341b8437afa0
技术云 <3260130869@qq.com>
2023-07-02 13:51:08 +0000

    [fixbug] 写错了
    Signed-off-by: 技术云 <3260130869@qq.com>

--- index.php
+++ index.php
@@ -3,36 +3,36 @@
 include 'config.php';
 
 if(isset($_GET['id'])){
-    die('缺少id参数');
-}
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
 
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+    if(! $conn )
+    {
+        die('连接失败: ' . mysqli_error($conn));
+    }
 
-if(! $conn )
-{
-    die('连接失败: ' . mysqli_error($conn));
-}
-
-mysqli_query($conn , "set names utf8");
+    mysqli_query($conn , "set names utf8");
  
-$sql = 'select num,  url, add_date from go_to_url where num="'.$_GET['id'].'"';
+    $sql = 'select num,  url, add_date from go_to_url where num="'.$_GET['id'].'"';
 
-mysqli_select_db( $conn, $dbname );
-$retval = mysqli_query( $conn, $sql );
+    mysqli_select_db( $conn, $dbname );
+    $retval = mysqli_query( $conn, $sql );
 
-if(! $retval )
-{
-    die('无法读取数据: ' . mysqli_error($conn));
+    if(! $retval )
+    {
+        die('无法读取数据: ' . mysqli_error($conn));
+    }
+
+    if($retval->num_rows ==0){
+        die('ID不存在！');
+    }
+
+    while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
+    {
+        header("Location: ".base64_decode($row['url']));
+    }
+
+    mysqli_close($conn);
+}else{
+    die('ID参数不存在');
 }
-
-if($retval->num_rows ==0){
-    die('ID不存在！');
-}
-
-while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
-{
-    header("Location: ".base64_decode($row['url']));
-}
-
-mysqli_close($conn);
 ?>
