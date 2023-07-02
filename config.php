996b84cc2bd8348107e393db998d504d464d21b2
技术云 <3260130869@qq.com>
2023-07-02 13:21:09 +0000

    [update] 缩进
    Signed-off-by: 技术云 <3260130869@qq.com>

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-//此文件为配置文件
-//请务必在运行install.php之前，填写本文件，删除install目录后，必须保留本文件，否则程序无法运行
-$dbhost = 'localhost';  // mysql服务器主机地址
-$dbuser = 'user_name';            // mysql用户名
-$dbpass = 'user_password';          // mysql用户名密码
-$dbname = 'dbbase_name';     //mysql数据库名称
-$admin_password = 'md5_text';   //admin管理面板的密码，需要填入密码的md5值
-$my_url = 'url';    //当前项目根目录网址，例如：http://xxx.com/short_url/、http://aaa.top/、https://b.cn/、http://a.b.com/s/，记得要加http/https协议，末尾加“/”
+    //此文件为配置文件
+    //请务必在运行install.php之前，填写本文件，删除install目录后，必须保留本文件，否则程序无法运行
+    $dbhost = 'localhost';  // mysql服务器主机地址
+    $dbuser = 'user_name';            // mysql用户名
+    $dbpass = 'user_password';          // mysql用户名密码
+    $dbname = 'dbbase_name';     //mysql数据库名称
+    $admin_password = 'md5_text';   //admin管理面板的密码，需要填入密码的md5值
+    $my_url = 'url';    //当前项目根目录网址，例如：http://xxx.com/short_url/、http://aaa.top/、https://b.cn/、http://a.b.com/s/，记得要加http/https协议，末尾加“/”
 ?>
