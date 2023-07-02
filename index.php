82cfcf5ea7723e4a70c183d6a15039ff96a57334
技术云 <3260130869@qq.com>
2023-07-02 13:46:10 +0000

    [update] 生产环境适配
    Signed-off-by: 技术云 <3260130869@qq.com>

--- index.php
+++ index.php
@@ -1,33 +1,38 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
-//header("content-type:text/html;charset=utf-8");
-
 include 'config.php';
 
+if(isset($_GET['id'])){
+    echo '缺少id参数';
+}
+
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
-//echo $_GET['id'];
+
 if(! $conn )
 {
     die('连接失败: ' . mysqli_error($conn));
 }
-// 设置编码，防止中文乱码
+
 mysqli_query($conn , "set names utf8");
  
 $sql = 'select num,  url, add_date from go_to_url where num="'.$_GET['id'].'"';
- 
+
 mysqli_select_db( $conn, $dbname );
 $retval = mysqli_query( $conn, $sql );
+
 if(! $retval )
 {
     die('无法读取数据: ' . mysqli_error($conn));
 }
-if($retval->num_rows ==0){//没有匹配的数据
+
+if($retval->num_rows ==0){
     echo 'ID不存在！';
 }
+
 while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
 {
-    //echo base64_decode($row['url']);
     header("Location: ".base64_decode($row['url']));
 }
+
 mysqli_close($conn);
 ?>
