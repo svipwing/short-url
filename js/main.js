282157ff0121d07a2e6e339b7488a109a87221f5
root <root@localhost>
2024-01-09 12:48:12 +0800

    [update] 界面优化

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+  re = /http/;
+  
+  $("#start").click(function(){
+    
+    if(re.test($('#link').val())){
+        
+      $.post("add.php",{'url':$('#link').val()},function(data,status){
+        $('message').text("短链接地址: " + data);
+      });
+      
+    }else{
+      swal("提示","链接不规范，必须使用http://或https://开头","error");
+    }
+    
+  });
+});
--- new.php
+++ new.php
@@ -1,25 +1,31 @@
 <?php header("content-type:text/html;charset=utf-8"); ?>
 
-<script src="js/jquery.js"></script>
+<html>
+<head>
 
-<script>
-$(function(){
-    re = /http/;
-    $("#b").click(function(){
-        if(re.test($('#t').val())){
-            $.post("add.php",{'url':$('#t').val()},function(data,status){
-                $('#aaa').text("短链接地址: " + data);
-            });
-        }else{
-            alert("链接不规范，必须使用http://或https://开头");
-        }
-    });
-});
-</script>
+  <title>短链接系统</title>
+  <script src="./js/jquery.js"></script>
+  <script src="./js/sw.js"></script>
+  
+  <link rel="stylesheet" href="./css/main.css" />
 
-<center>
-    <h1>你好，请添加短链接</h1>
-    <p>长链接：<input type='text' id='t' value='http://' /> </p>
-    <button id='b'>生成短链接</button>
-    <p id='aaa'></p>
-</center>
+</head>
+<body>
+  <script src="./js/main.js"></script>
+  
+  <center>
+    
+    <div>
+      <h1 style="margin-bottom:20px;">短链接系统</h1>
+    
+      <p style="margin-bottom:20px;">
+        长链接：<input type='text' id='link' value='http://' />
+      </p>
+    
+      <button id='start' style="margin-bottom:20px;">生成短链接</button>
+    
+      <p id='message'>您的短链接：http://请先生成短链接.com</p>
+    </div>
+  </center>
+</body>
+</html>
