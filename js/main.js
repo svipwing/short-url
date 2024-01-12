f44ccd943947b3540a65abcb3c6b681205b66f4c
root <root@localhost>
2024-01-12 20:00:10 +0800

    [update] UI优化和暗黑模式

--- js/main.js
+++ js/main.js
@@ -6,12 +6,32 @@ $(function(){
     if(re.test($('#link').val())){
         
       $.post("add.php",{'url':$('#link').val()},function(data,status){
-        $('#message').text("短链接地址: " + data);
+        $('message').text("短链接地址: " + data);
       });
       
     }else{
       swal("提示","链接不规范，必须使用http://或https://开头","error");
     }
     
+  });
+  
+  function loadUI(){
+    $("#dark").text($.cookie("ui")=="true"?"亮色模式":"暗色模式");
+    if($.cookie("ui") === undefined){
+      $.cookie("ui", "false");
+    }
+    if ($.cookie("ui") == "true") {
+      $("body").addClass("dark");
+    }else{
+      $("body").removeClass("dark");
+    }
+  }
+  
+  loadUI();
+  
+  $("#dark").click(function(){
+    $.cookie("ui", $.cookie("ui")=="true"?"false":"true");
+      
+    loadUI();
   });
 });
--- new.php
+++ new.php
@@ -6,23 +6,27 @@
   <title>短链接系统</title>
   <script src="./js/jquery.js"></script>
   <script src="./js/sw.js"></script>
+  <script src="./js/cookie.js"></script>
   
   <link rel="stylesheet" href="./css/main.css" />
+  <link rel="stylesheet" href="./css/grid.css" />
 
 </head>
 <body>
   <script src="./js/main.js"></script>
   
+  <button id="dark" class="btn" style="position:absolute;bottom:0;right:0;font-size:10px;">暗黑模式</button>
+  
   <center>
     
-    <div>
+    <div class="card">
       <h1 style="margin-bottom:20px;">短链接系统</h1>
     
       <p style="margin-bottom:20px;">
         长链接：<input type='text' id='link' value='http://' />
       </p>
     
-      <button id='start' style="margin-bottom:20px;">生成短链接</button>
+      <button id='start' style="margin-bottom:20px;" class="color-blue-full">生成短链接</button>
     
       <p id='message'>您的短链接：http://请先生成短链接.com</p>
     </div>
