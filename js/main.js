04d5aeb94828e783731b8edfbcb8c2bdc2aa3a0c
root <root@localhost>
2024-01-12 22:23:44 +0800

    [update] 自适应暗黑

--- js/main.js
+++ js/main.js
@@ -15,12 +15,20 @@ $(function(){
     
   });
   
+  function checkDarkMode() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
   function loadUI(){
     $("#dark").text($.cookie("ui")=="true"?"亮色模式":"暗色模式");
     if($.cookie("ui") === undefined){
       $.cookie("ui", "false");
     }
-    if ($.cookie("ui") == "true") {
+    if ($.cookie("ui") == "true" || checkDarkMode()) {
       $("body").addClass("dark");
     }else{
       $("body").removeClass("dark");
